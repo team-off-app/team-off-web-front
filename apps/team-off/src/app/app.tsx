2da92e31be7e5ad2250d1a1504d45f17022c3813
Richard Lopes <richard.lopes92@gmail.com>
2023-10-14 12:43:24 -0300

    feat: Add UI elements

--- apps/team-off/src/app/app.tsx
+++ apps/team-off/src/app/app.tsx
@@ -1,6 +1,20 @@
-import { Container } from '@mui/material';
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
 import cn from 'classnames';
 
 export function App() {
-  return <Container>teste</Container>;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Container>
+            <Typography variant="h6">Team Off</Typography>
+          </Container>
+        </Toolbar>
+      </AppBar>
+
+      <Container>
+        <Typography variant="h1">teste</Typography>
+      </Container>
+    </>
+  );
 }
--- apps/team-off/src/main.tsx
+++ apps/team-off/src/main.tsx
@@ -1,18 +1,36 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import { App } from './app/app';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <App />
+    <CssBaseline />
+
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
