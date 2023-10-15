297d7d93936942729724de61fcb4e10149ba8c59
Richard Lopes <richard.lopes92@gmail.com>
2023-10-15 11:21:45 -0300

    chore: Add typescript plugin

--- @team-off/calendar/src/lib/calendar.tsx
+++ @team-off/calendar/src/lib/calendar.tsx
@@ -8,7 +8,9 @@ export function Calendar(props: CalendarProps) {
   return (
     <div className={styles['container']}>
       <Box display="grid" gridTemplateColumns="repeat(10, 1fr)">
-        <Box>Welcome to Calendar!</Box>
+        {range().map((i) => (
+          <Box key={i}>Welcome to Calendar!</Box>
+        ))}
       </Box>
     </div>
   );
