1652582d1c417bff724206c7b153252aeb835738
Richard Lopes <richard.lopes92@gmail.com>
2023-10-29 02:58:12 -0300

    chore: Improve colors and positioning

--- @team-off/calendar/src/lib/calendar.tsx
+++ @team-off/calendar/src/lib/calendar.tsx
@@ -1,61 +1,80 @@
-import { Box, Paper, Typography } from '@mui/material';
+import { Avatar, Box, Paper, Typography } from '@mui/material';
+import { lightBlue } from '@team-off/theme';
 import { range } from 'lodash';
 import { Fragment } from 'react';
 
 /* eslint-disable-next-line */
 export interface CalendarProps {}
 
-const n = 10;
+const borderColor = lightBlue['100'];
+const bgColor = lightBlue['50'];
 
-const borderColor = 'grey.400';
+const months = [
+  {
+    name: 'Janeiro',
+    days: 5,
+  },
+  {
+    name: 'Fevereiro',
+    days: 5,
+  },
+  {
+    name: 'Março',
+    days: 2,
+  },
+];
 
-const daysPerMonth = 5;
+const days = months.reduce((acc, month) => acc + month.days, 0);
+
+const searchBoxSpan = ['MONTHS', 'DAYS'].length;
 
 export function Calendar(props: CalendarProps) {
   return (
     <Box
       display="grid"
-      gridTemplateColumns={`repeat(${15}, 60px)`}
+      gridTemplateColumns={`300px repeat(${days}, minmax(70px, auto))`}
       overflow="auto"
     >
       <Box
-        p={1}
-        gridColumn={`span ${daysPerMonth}`}
+        p={2}
         textAlign="center"
-        border="1px solid"
+        sx={{
+          bgcolor: bgColor,
+          borderTopLeftRadius: 6,
+          borderTopRightRadius: 6,
+        }}
+        gridRow={`span ${searchBoxSpan}`}
+        mr={2}
+        borderBottom="1px solid"
         borderColor={borderColor}
-      ></Box>
+      >
+        Search box
+      </Box>
 
-      {['March', 'April'].map((month, index) => (
+      {months.map((month, index) => (
         <Box
-          key={month}
+          key={month.name}
           p={1}
-          gridColumn={`span ${daysPerMonth}`}
+          gridColumn={`span ${month.days}`}
           textAlign="center"
           border="1px solid"
+          borderLeft={index > 0 ? 'none' : '1px solid'}
+          borderTop="none"
           borderColor={borderColor}
-          borderLeft="none"
         >
-          {month}
+          {month.name}
         </Box>
       ))}
 
-      <Box
-        gridColumn={`span ${daysPerMonth}`}
-        border="1px solid"
-        borderTop="none"
-        borderColor={borderColor}
-      ></Box>
-
-      {range(n).map((index) => (
+      {range(days).map((index) => (
         <Box
           p={1}
           textAlign="center"
           key={index}
           border="1px solid"
           borderTop="none"
+          borderLeft={index > 0 ? 'none' : '1px solid'}
           borderColor={borderColor}
-          borderLeft="none"
         >
           <Typography fontWeight="medium">D{index}</Typography>
         </Box>
@@ -63,27 +82,43 @@ export function Calendar(props: CalendarProps) {
 
       {range(5).map((_, index) => (
         <Fragment key={index}>
-          <Box
-            gridColumn={`span ${daysPerMonth}`}
-            border="1px solid"
-            borderTop="none"
-            borderColor={borderColor}
-          >
-            eu
+          <Box bgcolor={bgColor} mr={2}>
+            <Box
+              display="flex"
+              alignItems="center"
+              gap={1}
+              borderBottom="1px solid"
+              borderColor={borderColor}
+              mx={2}
+              py={2}
+            >
+              <Avatar>H</Avatar>
+              <Box display="block">
+                <Typography variant="body1">Linda</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Digital marketing
+                </Typography>
+              </Box>
+            </Box>
           </Box>
-          {range(n).map((index) => (
+
+          {range(days).map((index) => (
             <Box
               p={1}
               textAlign="center"
               key={index}
               border="1px solid"
               borderTop="none"
+              borderLeft={index > 0 ? 'none' : '1px solid'}
+              borderBottom="none"
               borderColor={borderColor}
-              borderLeft="none"
               position="relative"
+              display="flex"
+              justifyContent="stretch"
+              alignItems="center"
             >
-              <Paper sx={{ display: 'flex', zIndex: 1 }}>
-                <Box width={2} bgcolor="tomato" m={1} borderRadius="2px" />
+              <Paper sx={{ display: 'flex', zIndex: 1, flex: 1 }}>
+                <Box width="3px" bgcolor="tomato" />
                 <Typography p={1}>T</Typography>
               </Paper>
             </Box>
