119d2c7a2eb1d9bff0abb2e176e69313c05166f9
Richard Lopes <richard.lopes92@gmail.com>
2023-10-29 04:29:09 -0300

    feat: Add search field

--- @team-off/calendar/src/lib/components/data/SearchUsers.tsx
+++ @team-off/calendar/src/lib/components/data/SearchUsers.tsx
@@ -1,22 +1,36 @@
-import { Box } from '@mui/material';
-import { searchBoxSpan, bgColor, borderColor } from '../../constants';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { Box, Divider, IconButton, InputBase, Paper } from '@mui/material';
+import { bgColor, borderColor, searchBoxSpan } from '../../constants';
 
 export function SearchUsers() {
   return (
     <Box
       p={2}
       textAlign="center"
-      sx={{
-        bgcolor: bgColor,
-        borderTopLeftRadius: 6,
-        borderTopRightRadius: 6,
-      }}
+      sx={{ borderTopLeftRadius: 6, borderTopRightRadius: 6 }}
+      bgcolor={bgColor}
       gridRow={`span ${searchBoxSpan}`}
       mr={2}
       borderBottom="1px solid"
       borderColor={borderColor}
+      display="flex"
     >
-      Search box
+      <Paper
+        variant="outlined"
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      >
+        <IconButton sx={{ p: '10px' }} aria-label="search" disabled>
+          <SearchIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search user"
+          inputProps={{ 'aria-label': 'search user' }}
+        />
+      </Paper>
     </Box>
   );
 }
