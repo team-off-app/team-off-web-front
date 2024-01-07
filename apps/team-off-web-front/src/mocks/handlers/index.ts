bebba0c3be4d282484a2d53943bd07869be6245c
Richard Lopes <richard.lopes92@gmail.com>
2024-01-07 15:53:48 -0300

    chore: Update msw url

--- apps/team-off-web-front/src/mocks/handlers/index.ts
+++ apps/team-off-web-front/src/mocks/handlers/index.ts
@@ -1,27 +1,28 @@
 import dayjs from 'dayjs';
 import { delay, http, HttpResponse } from 'msw';
+import { v4 } from 'uuid';
 
 export const handlers = [
-  http.get('/users', async () => {
+  http.get('/api/users/events', async () => {
     await delay();
     return HttpResponse.json([
       {
-        id: '871f9034-8de8-4254-b6b6-e980e1907e26',
+        id: v4(),
         name: 'Richard Lopes',
         photoUrl: 'richard-lopes.png',
         teams: [
           {
-            id: '0308acdf-5d03-4ef6-97df-f3c0effd6d7e',
+            id: v4(),
             name: 'Financeiro',
           },
           {
-            id: '532c3f3d-1657-436b-b5b7-f8231c9cfc77',
+            id: v4(),
             name: 'Gerencia',
           },
         ],
         events: [
           {
-            id: '196deb57-7e30-4603-b2ac-68904a23df42',
+            id: v4(),
             title: null,
             startDate: dayjs().startOf('day').subtract(5, 'day').toISOString(),
             endDate: dayjs().startOf('day').toISOString(),
@@ -29,7 +30,7 @@ export const handlers = [
             type: 'VACATION',
           },
           {
-            id: 'd3ab5468-f5eb-438e-9d5c-c29af6f56ac3',
+            id: v4(),
             title: null,
             startDate: dayjs().startOf('day').add(1, 'day').toISOString(),
             endDate: dayjs().startOf('day').add(2, 'day').toISOString(),
@@ -39,22 +40,22 @@ export const handlers = [
         ],
       },
       {
-        id: '6c97dcd8-5d44-4042-876b-2d753f9847e0',
+        id: v4(),
         name: 'Rodrigo Barbosa',
         photoUrl: 'rodrigo-barbosa.png',
         teams: [
           {
-            id: '6ad1a1e4-acef-4f94-a080-1372a0811c6c',
+            id: v4(),
             name: 'Marketing',
           },
           {
-            id: '24dd319a-d313-439a-b6a8-9aa033244e29',
+            id: v4(),
             name: 'Tecnologia',
           },
         ],
         events: [
           {
-            id: '3cc49fbd-808c-46a9-9cb7-c1576e6f6ded',
+            id: v4(),
             title: null,
             startDate: dayjs().startOf('day').subtract(7, 'day').toISOString(),
             endDate: dayjs().startOf('day').subtract(1, 'day').toISOString(),
@@ -62,7 +63,7 @@ export const handlers = [
             type: 'VACATION',
           },
           {
-            id: '1d68ab41-c142-41e0-8cb3-9cf81556b158',
+            id: v4(),
             title: null,
             startDate: dayjs().startOf('day').add(4, 'day').toISOString(),
             endDate: dayjs().startOf('day').add(8, 'day').toISOString(),
